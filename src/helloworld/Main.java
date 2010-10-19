e04a63b1b070fc5b0933ad8b8ac05edfa88c94a3
Hussam Alfaleh <hussam312@yahoo.com>
2010-10-19 14:16:02 +0300

    Adding hello world

--- src/helloworld/Main.java
+++ src/helloworld/Main.java
@@ -15,7 +15,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Hello World!");
     }
 
 }
