3f54232dd19683a624e41da416c254579761c3d9
Hussam Alfaleh <hussam312@yahoo.com>
2010-10-20 09:43:41 +0300

    Abdulaziz to Hussam

--- src/helloworld/Main.java
+++ src/helloworld/Main.java
@@ -16,10 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
 
-
-  
-
-      System.out.println("Hello World! (Abdulaziz)");
+      System.out.println("Hello World! (Hussam!!!)");
 
     }
 
