f6c10dae3af241fc581ee17a87fa230881023d9f
Abdulaziz Alobaid <aaalobaid@kacst.edu.sa>
2010-10-20 09:32:12 +0300

    Delete hussam print

--- src/helloworld/Main.java
+++ src/helloworld/Main.java
@@ -16,8 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        System.out.println("Hello World! (Azeez)");
+      System.out.println("Hello World! (Azeez)");
     }
 
 }
