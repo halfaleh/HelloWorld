fb30239d543ea05e82cffded969bc02f117814b3
Hussam Alfaleh <hussam312@yahoo.com>
2010-10-20 09:36:07 +0300

    Changing azzez to hussam

--- src/helloworld/Main.java
+++ src/helloworld/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
 
-      System.out.println("Hello World! (Azeez)");
+      System.out.println("Hello World! (Hussam)");
     }
 
 }
