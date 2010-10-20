48aa3ae8b16f7cc68e4967eb6692545b0c8a22e9
Abdulaziz Alobaid <aaalobaid@kacst.edu.sa>
2010-10-20 10:09:22 +0300

    Still trying2

--- src/helloworld/Main.java
+++ src/helloworld/Main.java
@@ -15,13 +15,12 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
+
 int x=2;
-=======
 
       System.out.println("Hello World! (Hussam!!!)");
 
->>>>>>> 01e66f08cb1bb1e66d9be9b1a0d8557feeb0e5ae
+
 
     }
 
